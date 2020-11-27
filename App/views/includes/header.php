3dc382aa6fa0e91e7903c011ca4701632cc17218
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-11-27 15:16:08 -0200

    header.php

---
+++ App/views/includes/header.php
@@ -0,0 +1,38 @@
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark " id="nav-color">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                <img src="img/logo.svg" height="30px" width="35px" class="mr-1">
+                PrevClima
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" 
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item ">
+                            <a class="nav-link nav-link-custon" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link nav-link-custon" href="#">Clima</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Chuva</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Mapas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Notícias</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-1 form-nav" type="search" placeholder="pesquisar cidade" aria-label="pesquisar">
+                        <button class="btn btn-custon" type="submit"> <i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </nav>      
+    </header>
