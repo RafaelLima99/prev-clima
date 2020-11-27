349e3d38f30f550fbd6d13268cfbe2c014ab5932
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-11-27 15:16:07 -0200

    footer.php

---
+++ App/views/includes/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <div class="rodape d-flex align-items-center justify-content-center">
+            <p class="text-light" >Todos direitos reservados &copy</p> 
+        </div>
+    </footer>
